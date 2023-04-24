0ff31f01bc01d4cab7bfb5090fda7381e97aa33b
Kyosuke Takenaka <smith.shimomura@gmail.com>
2023-04-24 11:19:42 +0900

    config: ストレージAPI、vite関係設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from '@crxjs/vite-plugin'
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "CRX Example",
+  version: "1.0.0",
+  action: {
+    default_popup: "index.html",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), crx({ manifest })],
 })
